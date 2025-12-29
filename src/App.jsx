ebcc9f88f4eb04a14ef181a3581fb914a55deffe
fatmaayad13 <fatma.khaled.ayad@gmail.com>
2025-12-29 02:48:30 -0500

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar';
-import { Intro } from "./components/Intro/Intro";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
-
+import { Intro } from './components/Intro/intro';
 
 function App() {
   return (
