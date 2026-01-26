ab78fc8ef3c94e2945dbe2a5e6d8a0d26d14a1c1
fatmaayad13 <fatma.khaled.ayad@gmail.com>
2026-01-26 01:49:09 -0500

    updated projects

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -25,7 +25,7 @@ export const ProjectCard = ({
       </ul>
       <div className={styles.links}>
         <a href={demo} className={styles.link}>
-          Demo
+          {demoLabel ?? "Demo"}
         </a>
         <a href={source} className={styles.link}>
           Source
