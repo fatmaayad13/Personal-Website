eb83b6e1cb0e96ced88493c64ae946408166d521
fatmaayad13 <fatma.khaled.ayad@gmail.com>
2026-01-26 01:50:56 -0500

    updated projects

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./ProjectCard.module.css";
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
+  project: { title, imageSrc, description, skills, demo, source, demoLabel},
 }) => {
   return (
     <div className={styles.container}>
